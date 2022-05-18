e20060a0fffe0e3e0fc10701cc036dd586928da4
ibimongit <104595290+ibimongit@users.noreply.github.com>
2022-05-18 23:07:38 +0100

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -29,10 +29,14 @@ model.fit(X_train, y_train)
 explainer = ClassifierExplainer(model, X_test, y_test)
 ExplainerDashboard(explainer).run()
 
-st.title("This is the machine learning page")
+def app():
+    """
+    Set appearance to wide mode.
+    """
+    st.title("This is the machine learning page")
 
-dashboardurl = 'http://10.12.91.165:8050'
-st.components.v1.iframe(dashboardurl, width=None, height=900, scrolling=True)
+    dashboardurl = 'http://127.0.0.1:8050/'
+    st.components.v1.iframe(dashboardurl, width=None, height=900, scrolling=True)
 
 
 # In[ ]:
